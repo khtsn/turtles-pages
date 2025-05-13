119391aab69d3531d0f9cfcd1c632bacb0af9efc
KhanhTSN <khanhtsn@gmail.com>
2025-05-13 10:48:46 +0700

    feat: add proxy

--- reveal/index.js
+++ reveal/index.js
@@ -7,36 +7,26 @@ const nftAddress = "0x2baa455e573df4019b11859231dd9e425d885293"
 const provider = new ethers.JsonRpcProvider("https://evm.cronos.org/")
 const abi = require('./abi.json')
 const contract = new Contract(nftAddress, abi, provider)
+const proxy = require('express-http-proxy');
 
 const getMetadata = async (req, res) => {
-    try {
-        const tokenId = parseInt(req.params['id']) + 1
-        const totalSupply = await contract.totalSupply()
-        if (tokenId > totalSupply) {
-            res.status(404).send({})
-        }
-        const url = await fetch(storageURL + "/" + tokenId + ".json")
-        res.send(await url.json())
-    } catch {
+    const tokenId = parseInt(req.params['id']) + 1
+    const totalSupply = await contract.totalSupply()
+    if (tokenId > totalSupply) {
         res.status(404).send({})
     }
+    const url = await fetch(storageURL + "/" + tokenId + ".json")
+    res.send(await url.json())
 }
 
-app.get('/:id.png', async (req, res) => {
-    try {
-        const tokenId = parseInt(req.params['id'])
-        const totalSupply = await contract.totalSupply()
-        if (tokenId > totalSupply) {
-            res.status(404).send({})
-        }
-        const url = await fetch(storageURL + "/" + tokenId + ".png")
-        res.setHeader('content-type', 'image/png');
-        res.setHeader('cache-control', 'max-age=5184000')
-        res.send(await url.bytes())
-    } catch {
+app.get('/:id.png', async (req, res, next) => {
+    const tokenId = parseInt(req.params['id'])
+    const totalSupply = await contract.totalSupply()
+    if (tokenId > totalSupply) {
         res.status(404).send({})
     }
-})
+    next()
+}, proxy(storageURL))
 app.get('/:id.json', getMetadata)
 app.get('/:id', getMetadata)
 
